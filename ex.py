715a344f1926305f14093e57230bf6d60263a041
Adil Zhumkeshov <43197301+AdilAdilovich@users.noreply.github.com>
2019-12-27 20:49:31 +0600

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,16 @@
+{
+  "titles": [
+    "adil.zhumkeshov14@gmail.com",
+    "it-projects"
+  ],
+  "emails": [
+    "k.damir021@gmail.com",
+    "sabitkaraev@gmail.com"
+  ],
+  "questions":[
+    "Билет1",
+    "Билет2",
+    "Билет3",
+    "Билет4"
+  ]
+}
